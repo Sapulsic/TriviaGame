84adb9fc4c1f5174a7e8ecbecec977ca149cd184
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-17 21:35:44 -0500

    added code

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+// Global Variables
+//  ============================================
+// Arrays and Variables for initiating Data
+var $wins = $('#wins');
+var $loss = $('#loss');
+var $startGame = $('#startGame');
+var $winCount = $('winCount');
+
+var $timeRemain = $('#timeRemain');
+var $question = $('#question');
+var $answers = $('#answers');
+
+// Counters
+// var winCounter = 0;
+// var lossCounter = 0;
+
+// Random
+// var airRandom = Math.floor(Math.random()* 12) + 1;
+// var earthRandom = Math.floor(Math.random()* 12) + 1;
+// var waterRandom = Math.floor(Math.random()* 12) + 1;
+// var fireRandom = Math.floor(Math.random()* 12) + 1;
+
+// var random = Math.floor(Math.random() * 120) + 19;
+
+// Functions
+//  ============================================
+$(document).ready(function (){
+
+
+
+    // Testing / Debugging
+
+    // Main Process
+    //  ============================================
+
+
+});

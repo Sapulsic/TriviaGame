4873296a14015e899c93c1f65a80c38ba606c708
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-19 02:20:13 -0500

    made changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,9 +13,13 @@ var $choices = $('#choices');
 var $correctAnswers = $('#correctAnswers');
 var $incorrectAnswers = $('#incorrectAnswers');
 
-var questionOne = [];
-var questionTwo = [];
-var questionThree = [];
+var questions = [
+    "What is the name of the movie about a young warrior looking for a cure, a lady and her proud clan of humans, a young girl and her relationships with the animals of the forest?",
+    "What is the name of this animated film about a young girl's own 'mailing company' in a new town, a journey to become a witch, and a chatty cat?",
+    "Name this Miyazaki masterpiece about a story of a seaside kingdom and another story a thousand years after a great war. A courageous princess and the 'Ohmu' are the main characters.",
+    "What movie is about a story of a mining apprentice, a legendary castle, air pirates, secret agents, and a luminous crystal?",
+];
+var answers = [];
 
 // Counters
 var winCounter = 0;
